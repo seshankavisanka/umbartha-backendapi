9bcde3a79799bbf2500eef0a6a100ce4caddcaf3
seshankavisanka <seshankavisanka@gmail.com>
2023-07-04 15:01:20 +0530

    create counselors module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CounselorsModule } from './modules/counselors/counselors.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.DB_URI),
+    CounselorsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
