d2d720bc196cfde6e8abe71efc15ba3dbf827c2c
seshankavisanka <seshankavisanka@gmail.com>
2023-07-05 11:51:14 +0530

    create health module and update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,9 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CounselorsModule } from './modules/counselors/counselors.module';
 import { ServicesModule } from './modules/services/services.module';
-import { LicensesModule } from './modules/licenses/licenses.module';
+import { EmailsModule } from './modules/emails/emails.module';
+import { FilesModule } from './modules/files/files.module';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
   imports: [
@@ -16,7 +18,9 @@ import { LicensesModule } from './modules/licenses/licenses.module';
     MongooseModule.forRoot(process.env.DB_URI),
     CounselorsModule,
     ServicesModule,
-    LicensesModule,
+    EmailsModule,
+    FilesModule,
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
